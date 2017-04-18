6dc641054ea935c7cd9bb8aa668f7815ffec9a3d
Martin Bee <martbee88@gmail.com>
2017-04-18 20:03:56 +0000

    Properly assign methods.

--- src/board.js
+++ src/board.js
@@ -2,7 +2,6 @@ import _ from 'lodash';
 
 export default function createBoard(size, gridSize) {
   return {
-    currentColor: this.BLACK,
     size,
     board: createGrid(size),
     gridSize,
@@ -12,141 +11,143 @@ export default function createBoard(size, gridSize) {
     EMPTY: 0,
     BLACK: 1,
     WHITE: 2,
+  };
+}
 
-    // Switches the current player
-    switchPlayer() {
-      this.currentColor = this.currentColor === this.BLACK ? this.WHITE : this.BLACK;
-    },
+createBoard.currentColor = createBoard.BLACK;
 
-    // Pass ability
-    pass() {
-      if (this.lastMovePassed) this.endGame();
+// Switches the current player
+createBoard.switchPlayer = function() {
+  this.currentColor = this.currentColor === this.BLACK ? this.WHITE : this.BLACK;
+}
 
-      this.lastMovePassed = true;
-      this.switchPlayer();
-    },
+// Pass ability
+createBoard.pass = function() {
+  if (this.lastMovePassed) this.endGame();
 
-    resetGame() {
-      this.currentColor = this.BLACK;
-      this.board = this.createGrid(this.size);
-      this.lastMovePassed = false;
-      this.inAtari = false;
-      this.attemptedSuicide = false;
-    },
+  this.lastMovePassed = true;
+  this.switchPlayer();
+}
 
-    // Called when the game ends (both players have passed)
-    endGame() {
-      alert("Game Over");
+createBoard.resetGame = function() {
+  this.currentColor = this.BLACK;
+  this.board = this.createGrid(this.size);
+  this.lastMovePassed = false;
+  this.inAtari = false;
+  this.attemptedSuicide = false;
+}
 
-      this.resetGame();
-    },
+// Called when the game ends (both players have passed)
+createBoard.endGame = function() {
+  alert("Game Over");
 
-    // Attempt to place a stone at (i,j). Returns true if the move was legal
-    play(i, j) {
-      console.log(`Played at ${i}, ${j}.`);
+  this.resetGame();
+}
 
-      this.attemptedSuicide = this.inAtari = false;
+// Given a board position, returns a list of [i, j] coordinates representing
+// orthagonally adjacent intersections
+createBoard.getAdjacentIntersections = function(i, j) {
+  const neighbors = [];
 
-      if (this.board[i][j] !== this.empty) return false;
+  if (i > 0) neighbors.push([i - 1, j]);
 
-      const color = this.board[i][j] = this.currentColor;
-      const captured = [];
-      const neighbors = this.getAdjacentIntersections(i, j);
-      let atari = false
+  if (j < this.size - 1) neighbors.push([i, j + 1]);
 
-      neighbors.forEach(neighbor => {
-        const state = this.board[neighbor[0]][neighbor[1]];
+  if (i < this.size - 1) neighbors.push([i + 1, j]);
 
-        if (state !== this.EMPTY && state !== color) {
-          const group = this.getGroup(neighbor[0], neighbor[1]);
-          const liberties = group.liberties;
+  if (j > 0) neighbors.push([i, j - 1]);
 
-          console.log(group);
+  return neighbors;
+}
 
-          if (liberties === 0)  {
-            captured.push(group);
-          } else if (liberties === 1)  {
-            atari = true;
-          }
-        }
-      });
+createBoard.getGroup = function(i, j) {
+  const { board } = this;
 
-      // detect suicide
-      if (_.isEmpty(captured) && this.getGroup(i, j)["liberties"] === 0) {
-        this.board[i][j] = this.EMPTY;
-        this.attemptedSuicide = true;
+  const color = board[i][j];
 
-        // Look more into this, do we need a bool return here??
-        return false;
-      }
+  if (color === this.EMPTY) return;
 
-      captured.forEach(group => {
-        group.stones.forEach(stone => this.board[stone[0]][stone[1]] = this.EMPTY);
-      });
+  const visited = {};
+  const visitedList = [];
+  const queue = [[i, j]];
+  let count = 0;
 
-      if (atari) this.inAtari = true;
+  while (queue.length > 0) {
+    const stone = queue.pop();
 
-      this.lastMovePassed = false;
-      this.switchPlayer();
+    if (visited[stone]) continue;
 
-      return true;
-    },
+    const neighbors = this.getAdjacentIntersections(stone[0], stone[1]);
 
-    // Given a board position, returns a list of [i, j] coordinates representing
-    // orthagonally adjacent intersections
-    getAdjacentIntersections(i, j) {
-      const neighbors = [];
+    // eslint-disable-next-line
+    neighbors.forEach(neighbor => {
+      const state = board[neighbor[0]][neighbor[1]];
 
-      if (i > 0) neighbors.push([i - 1, j]);
+      if (state === this.EMPTY) count ++
 
-      if (j < this.size - 1) neighbors.push([i, j + 1]);
+      if (state === color) queue.push([neighbor[0], neighbor[1]]);
+    });
 
-      if (i < this.size - 1) neighbors.push([i + 1, j]);
-
-      if (j > 0) neighbors.push([i, j - 1]);
-
-      return neighbors;
-    },
-
-    getGroup(i, j) {
-      const { board } = this;
-
-      const color = board[i][j];
-
-      if (color === this.EMPTY) return;
-
-      const visited = {};
-      const visitedList = [];
-      const queue = [[i, j]];
-      let count = 0;
-
-      while (queue.length > 0) {
-        const stone = queue.pop();
-
-        if (visited[stone]) continue;
-
-        const neighbors = this.getAdjacentIntersections(stone[0], stone[1]);
-
-        // eslint-disable-next-line
-        neighbors.forEach(neighbor => {
-          const state = board[neighbor[0]][neighbor[1]];
-
-          if (state === this.EMPTY) count ++
-
-          if (state === color) queue.push([neighbor[0], neighbor[1]]);
-        });
-
-        visited[stone] = true;
-        visitedList.push(stone);
-      }
-
-      return {
-        liberties: count,
-        stones: visitedList,
-      }
-    },
+    visited[stone] = true;
+    visitedList.push(stone);
   }
-};
+
+  return {
+    liberties: count,
+    stones: visitedList,
+  }
+}
+
+// Attempt to place a stone at (i,j). Returns true if the move was legal
+createBoard.play = function(i, j) {
+  console.log(`Played at ${i}, ${j}.`);
+
+  this.attemptedSuicide = this.inAtari = false;
+
+  if (this.board[i][j] !== this.empty) return false;
+
+  const color = this.board[i][j] = this.currentColor;
+  const captured = [];
+  const neighbors = this.getAdjacentIntersections(i, j);
+  let atari = false
+
+  neighbors.forEach(neighbor => {
+    const state = this.board[neighbor[0]][neighbor[1]];
+
+    if (state !== this.EMPTY && state !== color) {
+      const group = this.getGroup(neighbor[0], neighbor[1]);
+      const liberties = group.liberties;
+
+      console.log(group);
+
+      if (liberties === 0)  {
+        captured.push(group);
+      } else if (liberties === 1)  {
+        atari = true;
+      }
+    }
+  });
+
+  // detect suicide
+  if (_.isEmpty(captured) && this.getGroup(i, j)["liberties"] === 0) {
+    this.board[i][j] = this.EMPTY;
+    this.attemptedSuicide = true;
+
+    // Look more into this, do we need a bool return here??
+    return false;
+  }
+
+  captured.forEach(group => {
+    group.stones.forEach(stone => this.board[stone[0]][stone[1]] = this.EMPTY);
+  });
+
+  if (atari) this.inAtari = true;
+
+  this.lastMovePassed = false;
+  this.switchPlayer();
+
+  return true;
+}
 
 
 // Returns a size x size board with all entries set to this.Empty
